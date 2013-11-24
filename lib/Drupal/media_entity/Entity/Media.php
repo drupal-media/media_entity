ab474929508fe9a0e39c727f9b3050c3b82c609e
vladan.me <vladan.me@2640361.no-reply.drupal.org>
2013-11-24 14:43:01 +0100

    Issue #2128895 by vladan.me | slashrsm: Create media entity access controller.

--- lib/Drupal/media_entity/Entity/Media.php
+++ lib/Drupal/media_entity/Entity/Media.php
@@ -22,7 +22,7 @@ use Drupal\media_entity\MediaInterface;
  *   controllers = {
  *     "storage" = "Drupal\Core\Entity\FieldableDatabaseStorageController",
  *     "view_builder" = "Drupal\Core\Entity\EntityViewBuilder",
- *     "access" = "Drupal\Core\Entity\EntityAccessController",
+ *     "access" = "Drupal\media_entity\MediaAccessController",
  *     "form" = {
  *       "default" = "Drupal\Core\Entity\ContentEntityFormController",
  *       "delete" = "Drupal\Core\Entity\ContentEntityConfirmFormBase",
