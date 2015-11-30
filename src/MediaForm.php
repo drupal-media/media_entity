34a67369e9ef806866cf6ea61de27493777cbdae
felribeiro <felribeiro@1902796.no-reply.drupal.org>
2015-11-30 21:47:31 +0100

    Issue #2622106 by felribeiro: Revision option of media entity is not showing

--- src/MediaForm.php
+++ src/MediaForm.php
@@ -51,13 +51,56 @@ class MediaForm extends ContentEntityForm {
    * {@inheritdoc}
    */
   public function form(array $form, FormStateInterface $form_state) {
+    $media = $this->entity;
+    $account = $this->currentUser();
     $form = parent::form($form, $form_state);
 
     $form['advanced'] = array(
       '#type' => 'vertical_tabs',
-      '#attributes' => array('class' => array('entity-meta')),
       '#weight' => 99,
     );
+
+    // Add a log field if the "Create new revision" option is checked, or if the
+    // current user has the ability to check that option.
+    $form['revision_information'] = array(
+      '#type' => 'details',
+      '#title' => $this->t('Revision information'),
+      // Open by default when "Create new revision" is checked.
+      '#open' => $media->isNewRevision(),
+      '#group' => 'advanced',
+      '#attributes' => array(
+        'class' => array('media-form-revision-information'),
+      ),
+      '#weight' => 20,
+      '#access' => $media->isNewRevision() || $account->hasPermission('administer media'),
+    );
+
+    $form['revision_information']['revision'] = array(
+      '#type' => 'checkbox',
+      '#title' => $this->t('Create new revision'),
+      '#default_value' => $media->isNewRevision(),
+      '#access' => $account->hasPermission('administer media'),
+    );
+
+    // Check the revision log checkbox when the log textarea is filled in.
+    // This must not happen if "Create new revision" is enabled by default,
+    // since the state would auto-disable the checkbox otherwise.
+    if (!$media->isNewRevision()) {
+      $form['revision_information']['revision']['#states'] = array(
+        'checked' => array(
+          'textarea[name="revision_log"]' => array('empty' => FALSE),
+        ),
+      );
+    }
+
+    $form['revision_information']['revision_log'] = array(
+      '#type' => 'textarea',
+      '#title' => $this->t('Revision log message'),
+      '#rows' => 4,
+      '#default_value' => '',
+      '#description' => $this->t('Briefly describe the changes you have made.'),
+    );
+
     // Node author information for administrators.
     $form['author'] = array(
       '#type' => 'details',
@@ -112,6 +155,12 @@ class MediaForm extends ContentEntityForm {
    */
   public function save(array $form, FormStateInterface $form_state) {
     $media = $this->entity;
+
+    // Save as a new revision if requested to do so.
+    if (!$form_state->isValueEmpty('revision')) {
+      $media->setNewRevision();
+    }
+
     $media->save();
 
     if ($media->id()) {
