268d5c9f39545f3cad4da4371311dcb3daac1490
Phéna Proxima <djphenaproxima@gmail.com>
2015-12-03 12:09:31 -0500

    Created EmbedCodeValueTrait.

---
+++ src/EmbedCodeValueTrait.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\media_entity\EmbedCodeValueTrait.
+ */
+
+namespace Drupal\media_entity;
+
+use Drupal\Core\Field\FieldItemInterface;
+
+/**
+ * A trait to assist with handling external embed codes.
+ */
+trait EmbedCodeValueTrait {
+
+  /**
+   * Extracts the raw embed code from input which may or may not be wrapped.
+   *
+   * @param mixed $value
+   *   The input value. Can be a normal string or a value wrapped by the
+   *   Typed Data API.
+   *
+   * @return string|null
+   */
+  protected function getEmbedCode($value) {
+    if (is_string($value)) {
+      return $value;
+    }
+    elseif ($value instanceof FieldItemInterface) {
+      $class = $value->getFieldDefinition()->getClass();
+      $property = $class::mainPropertyName();
+      if ($property) {
+        return $value->get($property);
+      }
+    }
+  }
+
+}
