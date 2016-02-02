1bbaff912dd9591827e1d37d0b6c9d284a2fd5a0
Christoffer Palm <christoffer.palm@paax.se>
2016-02-02 13:26:36 +0100

    Issue #2661066 by NormySan, slashrsm: Expose content translation UI.

--- src/Entity/Media.php
+++ src/Entity/Media.php
@@ -253,7 +253,15 @@ class Media extends ContentEntityBase implements MediaInterface {
     $fields['langcode'] = BaseFieldDefinition::create('language')
       ->setLabel(t('Language code'))
       ->setDescription(t('The media language code.'))
-      ->setRevisionable(TRUE);
+      ->setTranslatable(TRUE)
+      ->setRevisionable(TRUE)
+      ->setDisplayOptions('view', array(
+        'type' => 'hidden',
+      ))
+      ->setDisplayOptions('form', array(
+        'type' => 'language_select',
+        'weight' => 2,
+      ));
 
     $fields['name'] = BaseFieldDefinition::create('string')
       ->setLabel(t('Media name'))
