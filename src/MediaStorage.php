c9a665d3c21a6574d3d6c130a4ff655ebccb7531
artem.kolotilkin <artem.kolotilkin@1915462.no-reply.drupal.org>
2016-09-30 14:14:42 +0100

    Issue #2802563 by temkin, dishabhadra, jcisio: Remove MediaStorage::onBundleRename().

--- src/MediaStorage.php
+++ src/MediaStorage.php
@@ -9,20 +9,4 @@ use Drupal\Core\Entity\Sql\SqlContentEntityStorage;
  */
 class MediaStorage extends SqlContentEntityStorage implements MediaStorageInterface {
 
-  /**
-   * {@inheritdoc}
-   */
-  public function onBundleRename($bundle, $bundle_new, $entity_type_id) {
-    parent::onBundleRename($bundle, $bundle_new, $entity_type_id);
-    // Update media entities with a new bundle.
-    $this->database->update('media')
-      ->fields(array('bundle' => $bundle_new))
-      ->condition('bundle', $bundle)
-      ->execute();
-    $this->database->update('media_field_data')
-      ->fields(array('bundle' => $bundle_new))
-      ->condition('bundle', $bundle)
-      ->execute();
-  }
-
 }
